eda91a670ade43f5c1b3e864c3f461dc24b256e1
kyramaz <kyramaz1@gmail.com>
2022-11-15 22:45:49 +0200

    pre-ready version lab_6 15.11.22 22:45

--- laba_6.cpp
+++ laba_6.cpp
@@ -16,36 +16,9 @@ int fill_Mat(int x, int y, int matrix[100][100])
     return matrix[x][y];
 }
 
-bool is_Prime(int x)
-{
-    for (int i = 2; i < sqrt(x); i++)
-    {
-        if (abs(x) % i == 0)
-        {
-            return false;
-        }
-    }
-    return true;
-}
-
-bool prime_numeral_exist(int x, int y, int matrix[100][100])
-{
-    for (int i = 0; i < x; i++)
-    {
-        for (int j = 0; j < y; j++)
-        {
-            if (is_Prime(matrix[i][j]))
-            {
-                return true;
-            }
-        }
-    }
-    return false;
-}
-
 bool Same_column(int x, int y, int matrix[100][100])
 {
-    int count{ 0 };
+    int count{0};
     for (int j = 0; j < y - 1; j++)
     {
         for (int k = 1; k < y; k++)
@@ -71,36 +44,58 @@ bool Same_column(int x, int y, int matrix[100][100])
     return false;
 }
 
+bool is_Prime(int x)
+{
+    for (int i = 2; i < sqrt(x); i++)
+    {
+        if (abs(x) % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool prime_numeral_exist(int x, int y, int matrix[100][100])
+{
+    for (int i = 0; i < x; i++)
+    {
+        for (int j = 0; j < y; j++)
+        {
+            if (is_Prime(matrix[i][j]) == true)
+            {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
 void sort_lines(int x, int y, int matrix[100][100])
 {
     int mas_sum_lines[100];
-    for (int i = 0; i < 100; i++)
-    {
-        mas_sum_lines[i] = 0;
-
-    }
+    int sum_line{0};
     for (int i = 0; i < x; i++)
     {
         for (int j = 0; j < y; j++)
         {
-            mas_sum_lines[i] += abs(matrix[i][j]);
+            sum_line += abs(matrix[i][j]);
         }
+        mas_sum_lines[i] = sum_line;
+        sum_line = 0;
     }
 
-    for (int i = 0; i < x; i++)
+    for (int i = 0; i < x-1; i++) // line number
     {
-        for (int j = 0; j < y; j++)
+        for (int j = 1; j < x; j++) // +to line number
         {
-            if (mas_sum_lines[i] > mas_sum_lines[j])
+            if (mas_sum_lines[i] > mas_sum_lines[i+j] && i+j < x)
             {
-                for (int k = 0; k < x-1; k++)
+                for (int k = 0; k < y; k++)
                 {
-                    for (int l = 0; l < y; l++)
-                    {
-                        swap(matrix[k][l], matrix[k + 1][l]);
-                    }
+                    swap(matrix[i][k], matrix[i + j][k]);
                 }
-                swap(mas_sum_lines[i], mas_sum_lines[j]);
+                swap(mas_sum_lines[i], mas_sum_lines[i+j]);
             }
         }
     }
@@ -122,17 +117,16 @@ int main()
     cin >> columns;
 
     fill_Mat(lines, columns, mat);
-
-    if (Same_column(lines,columns,mat) == true && prime_numeral_exist(lines,columns,mat) == true)
+    
+    if (Same_column(lines, columns, mat) && prime_numeral_exist(lines, columns, mat))
     {
-        sort_lines(lines,columns,mat);
+        sort_lines(lines, columns, mat);
     }
     else
     {
-        return -1;
+        printf("Не выполнены достаточные условия");
+        return 0;
     }
-
-
 
     for (int i = 0; i < lines; i++)
     {
@@ -142,6 +136,5 @@ int main()
         }
         cout << endl;
     }
-
 
 }
